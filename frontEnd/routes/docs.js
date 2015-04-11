b5cd674e734c18a4e3cce7ff6fb2eae6563a9551
jonroberts <jproberts00@gmail.com>
2015-04-11 17:24:00 -0400

    Updated server code and basic frontEnd

--- frontEnd/routes/docs.js
+++ frontEnd/routes/docs.js
@@ -5,10 +5,76 @@ var db = require("mongojs")
 
 exports.getDatasets = function (req,res){
 	var query=req.query.q;
+	var using=req.query.field;
+	var field=(using==undefined)?'keyword':using;
+	console.log(field);
+	var theQuery={};
+	theQuery[field]=query;
+
 	if (query==undefined) res.send({'error':'you must pass in a query, of form q='})
 	else{
-		db.datasets.find({'keyword':query},{'title':1},function(err,docs){
+		db.datasets.find(theQuery,{'title':1},function(err,docs){
 			res.send(docs);
 		})
 	}
+}
+
+exports.getCoOccuringKWs = function(req,res){
+	var query=req.query.q;
+	var using=req.query.field;
+	var field=(using==undefined)?'keyword':using;
+	var searches={'keyword':function(curr,result){
+				  		  		for(var kx in curr['keyword']){
+				  		  			var kw=curr['keyword'][kx];
+				  		  			if(result[kw]==undefined){
+				  		  				result[kw]=1;
+				  		  			}
+				  		  			else{
+				  		  				result[kw]+=1;
+				  		  			}
+				  		  		}
+				  		  	},
+				  	'description_textrank_kw':function(curr,result){
+				  		  		for(var kx in curr['description_textrank_kw']){
+				  		  			var kw=curr['description_textrank_kw'][kx];
+				  		  			if(result[kw]==undefined){
+				  		  				result[kw]=1;
+				  		  			}
+				  		  			else{
+				  		  				result[kw]+=1;
+				  		  			}
+				  		  		}
+				  		  	},
+				  	'description_bigram_kw':function(curr,result){
+				  		  		for(var kx in curr['description_bigram_kw']){
+				  		  			var kw=curr['description_bigram_kw'][kx];
+				  		  			if(result[kw]==undefined){
+				  		  				result[kw]=1;
+				  		  			}
+				  		  			else{
+				  		  				result[kw]+=1;
+				  		  			}
+				  		  		}
+				  		  	}
+				  	}
+				  	
+	var theQuery={'key':{},
+				  'cond':{},
+				  'reduce':searches[field],
+				  'initial':{}
+				 };
+	theQuery['cond'][field]={"$regex":new RegExp('^'+query, 'i')};
+
+	if (query==undefined) res.send({'error':'you must pass in a query, of form q='})
+	else{
+		db.datasets.group(theQuery,function(err,docs){
+			var values=docs[0];
+			var results=[];
+			for(var k in values) {results.push({'kw':k,'count':values[k]});}
+			results.sort(function(a,b){return +b.count-a.count;});
+
+			res.send(results);
+		})
+	}
+	
 }
--- frontEnd/server.js
+++ frontEnd/server.js
@@ -4,37 +4,16 @@ var express = require('express'),
 	setImmediate = global.setImmediate;
 
 var app = express();
-
-//root='/Users/Jon/Code/DataKind/GlobalGiving/nasaMining/frontEnd'
-
 app.get('/', function (req, res) {
 	//res.send('Hello world')
 	res.sendfile('public/index.html');
 });
 app.get('/getDatasets', docs.getDatasets);
-
-/*app.get('/^(.+)$/', function(req,res){
-	res.sendFile(root+'/public/' + req.params[0]);
-});*/
-
-/*
-var allowCrossDomain = function(req, res, next){
-	res.header('Access-Control-Allow-Origin', "*");
-	res.header('Access-Control-Allow-Methods', "GET,POST");
-	res.header('Access-Control-Allow-Headers', "Content-Type");
-}
-app.use(allowCrossDomain);
-app.use(bodyParser({'limit':'50mb'}));
-
-
-app.get('/', function(req,res){
-	res.sendfile('public/index.html');
-});
-
-app.get('/^(.+)$/', function(req,res){
+app.get('/getCoOccuringKWs', docs.getCoOccuringKWs)
+app.get(/^(.+)$/, function(req,res){
 	res.sendfile('public/' + req.params[0]);
 });
-*/
+
 var server = app.listen(3000, '127.0.0.1', function () {
 
   var host = server.address().address;
