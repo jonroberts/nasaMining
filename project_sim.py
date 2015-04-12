698b36c587584342775807e6e56b9238af499db2
flava flav <flavaflav@daddy-mac.local>
2015-04-12 15:30:02 -0400

    similarity output

--- project_sim.py
+++ project_sim.py
@@ -3,23 +3,24 @@ from numpy import column_stack
 from itertools import combinations, product
 
 if __name__ == '__main__':
-#     sim_data = json.load(open('data/keyword_synset_scores.json'))
+    sim_data = json.load(open('data/keyword_synset_scores.json'))
     data = json.load(open('data/nasa_kw.json'))
     outfile = 'data/project_similarity_scores.json'
     dataset = data['dataset']
     kw_field = 'keyword'
+    max_items = 500 # limit project pairs to analyse. there are ~16k projects, that's ~73 million pairs!
     sim_scores = {}
     proj_sims = []
 
     # setup sim score hash
-#     for i, ds in enumerate(sim_data):
-#         pair = ds['keyword']
-#         sl_pair = sorted([x.lower() for x in pair], key=unicode.lower)
-#         sim_scores[sl_pair] = ds['score']
+    for i, ds in enumerate(sim_data):
+        pair = ds['keyword']
+        sl_pair = str(sorted([x.lower() for x in pair], key=unicode.lower))
+        sim_scores[sl_pair] = ds['score']
         
     # calculate similarity between projects
     # score = the sum of all similarity scores for each pair of keyword words 
-    for proj_pair in combinations(dataset, 2):
+    for proj_pair in list(combinations(dataset, 2))[:max_items]:
         
         # a fancy way to get the individual words from all the keywords in a list
         kw1 = [item for sublist in [x.lower().split() for x in proj_pair[0][kw_field]] for item in sublist]
